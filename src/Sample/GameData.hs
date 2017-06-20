244be3acd5bda6f83db2b744516656ff4b152d0c
drobnik <karolinadrobnik@gmail.com>
2017-06-20 15:20:40 +0200

    Clean up imports and defined detailed export for GameData and GameState modules. Documentation and commentary for them in progress. Add new Travis configuration file -- hopefully it will work.

--- src/Sample/GameData.hs
+++ src/Sample/GameData.hs
@@ -1,111 +1,176 @@
 {-# LANGUAGE BangPatterns #-}
-module GameData where
 
-import SDL (V2 (..), Point(..), Rectangle(..))
-import qualified SDL
-import Engine.Datas
-import Engine.Consts
+module GameData
+  (
+    TileMap(..)
+  , Tile(..)
+  , TileKind(..)
+  , UnitKind(..)
+  , isEmpty
+  , empty
+  , getModels
+  , getTilesBox
+  , modelPosition
+  , unitPath
+  , tilesData
+  , tilePath
+  ) where
+
+import Data.Int(Int32)
+import Foreign.C.Types(CInt(..))
 import qualified Data.Set as Set (map, elems)
-import Render.Model (modifyModelPos, renPos, RenderModel(..))
+
+import qualified SDL
+import SDL (V2 (..)
+           , Point(..)
+           , Rectangle(..)
+           )
+
+import Engine.Collision (Collidable(..)
+                        , BoundingBox(BoundingBox)
+                        , BoxKind(..)
+                        )
+import Engine.Consts
+import Engine.Datas
+import Render.Model (modifyModelPos
+                    , renPos
+                    , RenderModel(..)
+                    )
 import Render.Primitives
-import Data.Int
-import Foreign.C.Types
-import Engine.Collision (Collidable(..), BoundingBox(BoundingBox)
-                        , BoxKind(..))
 
-import qualified Debug.Trace as D
---game specific directions + commands
-data Direction = LeftDir | RightDir | UpDir | DownDir | Unknown | End
-  deriving (Show, Eq, Ord)
+-- | Game specific directions data
+data Direction = LeftDir
+               | RightDir
+               | UpDir
+               | DownDir
+               | End
+               | Unknown
+               deriving (Show, Eq, Ord)
 
-data TileKind = Sky | Ground | Lava | Spikes
-  deriving (Show, Eq, Ord)
+-- | Tile sprite kinds enum
+data TileKind = Sky
+              | Ground
+              | Lava
+              | Spikes
+              deriving (Show, Eq, Ord)
 
+-- | Tile entity data.
 data Tile = Tile
-            { dim :: (Int32, Int32)
-            , pos :: (Int32, Int32)
-            , kind :: TileKind
-            , model :: RenderModel
-            , tBox :: BoundingBox
+            { dim   :: (Int32, Int32) -- ^ Dimensions of the tile
+            , pos   :: (Int32, Int32) -- ^ Position of the tile
+            , kind  :: TileKind       -- ^ Kind of the sprite
+            , model :: RenderModel    -- ^ Graphic representation
+            , tBox  :: BoundingBox    -- ^ Bounding box for collision detection
             } deriving (Ord)
 
 instance Collidable Tile where
   boundingBox = tBox
 
-instance Show Tile where
-  show (Tile dim pos kind _ _) = "Tile| dimens:" ++ show dim ++ ", pos:"
-                             ++ show pos ++ ", kind:" ++ show kind ++ "\t"
 instance Eq Tile where
   x == y = GameData.pos x == GameData.pos y
 
+-- | Data of a tile set for particular level
 data TileMap = TileMap
-               { width :: Int
-               , height :: Int
-               , tiles :: [Tile]
-               , tilesPath :: FilePath
+               { width     :: Int      -- ^ Width of a tile level
+               , height    :: Int      -- ^ Height of a tile level
+               , tiles     :: [Tile]   -- ^ List of tiles
+               , tilesPath :: FilePath -- ^ Path to tile map configuration
                } deriving(Eq)
 
-isEmpty :: TileMap -> Bool --maybe a new typeclass /later/
-isEmpty s = s == empty
-
-empty :: TileMap
-empty = TileMap{width=0, height=0, tiles=[], tilesPath=""}
-
-instance Show TileMap where
-  show (TileMap _ _ tiles _ ) = show tiles
-
-getModels :: [Tile]-> [RenderModel] -> [RenderModel]
-getModels (x:xs) rm = getModels xs ((renderModel x):rm)
-getModels [] rm = rm
-
-renderModel :: Tile -> RenderModel
-renderModel (Tile _ _ _ mod _) = mod
+-- | Data of entities in the game world
+data UnitKind = CoinU     -- ^ Collectable
+              | GateU     -- ^ LevelExit
+              | HealthUpU -- ^ Health enhanment
+              | PlayerU   -- ^  Enemy
+              deriving (Show, Eq, Ord)
 
 type TileSize = V2 Int32
-type TileTexture = (TileSize, [((Rectangle Int32), TileKind)])
+type TileTexture = (TileSize
+                   , [((Rectangle Int32) , TileKind)]
+                   )
 
-getTilesBox :: TileMap -> [(BoundingBox, BoxKind)]
-getTilesBox t = zip boxes kinds
-  where tiles' = filter(\x -> kind x /= Sky) (tiles t)
-        (boxes, kinds) = mapTiles tiles' ([], [])
-
-mapTiles :: [Tile] -> ([BoundingBox], [BoxKind]) -> ([BoundingBox], [BoxKind])
-mapTiles (t:ts) (boxes, kinds) = mapTiles ts ((b:boxes),(k:kinds))
-  where (b, k) = tileBoxKind t
-mapTiles [] boxes = boxes
-
-tileBoxKind :: Tile -> (BoundingBox, BoxKind)
-tileBoxKind (Tile _ _ kind _ tbox) = (tbox, check kind)
-  where check kind = case kind of
-                 Ground -> TileGround
-                 Lava -> TileLava
-                 Spikes -> TileSpikes
--- temp!!
+-- |Temporary function. Returns a path for tile sprites
 tilePath :: FilePath
 tilePath = "example_data/tiles.bmp"
 
-data UnitKind = CoinU | GateU | HealthUpU | PlayerU --  Enemy
-  deriving (Show, Eq, Ord)
---lame
-unitPath :: UnitKind -> FilePath
+-- |Temporary function. Returns paths for game resources, based
+-- on the UnitKind enum
+unitPath :: UnitKind
+         -> FilePath
 unitPath CoinU = "example_data/coin.bmp"
 unitPath HealthUpU = "example_data/health.bmp"
 unitPath GateU = "example_data/gate.bmp"
 unitPath PlayerU = "example_data/dot.bmp"
 
+-- | ..used in GameState:80
+isEmpty :: TileMap -> Bool
+isEmpty s = s == empty
+
+-- |
+empty :: TileMap
+empty = TileMap
+        { width = 0
+        , height = 0
+        , tiles = []
+        , tilesPath = ""
+        }
+
+-- |
+getModels :: [Tile]
+          -> [RenderModel]
+          -> [RenderModel]
+getModels (x:xs) rm = getModels xs ((renderModel x):rm)
+getModels [] rm = rm
+
+-- |
+renderModel :: Tile
+            -> RenderModel
+renderModel (Tile _ _ _ mod _) = mod
+
+-- |
+getTilesBox :: TileMap
+            -> [(BoundingBox, BoxKind)]
+getTilesBox t = zip boxes kinds
+  where tiles' = filter(\x -> kind x /= Sky) (tiles t)
+        (boxes, kinds) = mapTiles tiles' ([], [])
+
+-- |
+mapTiles :: [Tile]                     -- ^ List of tiles in the level
+         -> ([BoundingBox], [BoxKind]) -- ^ Partial list of bounding boxes
+         -> ([BoundingBox], [BoxKind]) -- ^ List of boundig boxes in the level
+mapTiles (t:ts) (boxes, kinds) = mapTiles ts ((b:boxes),(k:kinds))
+  where (b, k) = tileBoxKind t
+mapTiles [] boxes = boxes
+
+-- |
+tileBoxKind :: Tile -> (BoundingBox, BoxKind)
+tileBoxKind (Tile _ _ kind _ tbox) = (tbox, check kind)
+  where check kind = case kind of
+                       Ground -> TileGround
+                       Lava   -> TileLava
+                       Spikes -> TileSpikes
+
+-- | Setting a sprite frame for tiles texture file.
+-- Values are file specific and need to be configurated manually.
 tilesData :: TileKind -> Rectangle CInt
 tilesData k = case k of
-  Sky -> (Rectangle (P $ V2 0 0)) tile
-  Ground -> (Rectangle (P $ V2 32 0)) tile
-  Lava -> (Rectangle (P $ V2 64 0)) tile
-  Spikes -> (Rectangle (P $ V2 96 0)) tile
+                Sky    -> (Rectangle (P $ V2 0 0)) tile
+                Ground -> (Rectangle (P $ V2 32 0)) tile
+                Lava   -> (Rectangle (P $ V2 64 0)) tile
+                Spikes -> (Rectangle (P $ V2 96 0)) tile
   where d = CInt tileSize
         tile = V2 d d
 
-transformSet :: ActiveKeys -> (SDL.Keycode -> Direction) -> [Direction]
+-- | Translates a set of pressed keys to in-game player directions
+transformSet :: ActiveKeys                 -- ^ Pressed keys set
+             -> (SDL.Keycode -> Direction) -- ^ Custom mapping function
+             -> [Direction]                -- ^ List of directions
 transformSet keys f = Set.elems $ Set.map f keys
 
-transformKeys :: SDL.Keycode -> Direction
+-- | Simple mapping function from SDL.Keycode to in-game directions
+-- which understands arrow keys and escape presses.
+transformKeys :: SDL.Keycode -- ^ Key represented in SDL library
+              -> Direction   -- ^ In-game direction
 transformKeys (SDL.KeycodeUp) = UpDir
 transformKeys (SDL.KeycodeLeft)  = LeftDir
 transformKeys (SDL.KeycodeRight) = RightDir
@@ -113,17 +178,32 @@ transformKeys (SDL.KeycodeDown)  = DownDir
 transformKeys (SDL.KeycodeEscape) = End
 transformKeys _ = Unknown
 
-transDirection :: (Double, Double) -> [Direction] -> Double -> CenterPosition
+-- |
+transDirection :: (Double, Double) -- ^
+               -> [Direction]      -- ^
+               -> Double           -- ^
+               -> CenterPosition   -- ^
 transDirection (x',y') (x:xs) dt  = case x of
-  LeftDir -> transDirection ((x' - pVelo*dt), y') xs dt
-  RightDir -> transDirection ((x' + pVelo*dt), y') xs dt
-  UpDir -> transDirection (x', (y' - pVelo*dt)) xs dt
-  DownDir -> transDirection (x', (y' + pVelo*dt)) xs dt
-  _ ->  transDirection (x',y') xs dt
+                                      LeftDir  -> transDirection
+                                                  ((x' - pVelo*dt), y')
+                                                  xs dt
+                                      RightDir -> transDirection
+                                                  ((x' + pVelo*dt), y')
+                                                  xs dt
+                                      UpDir    -> transDirection
+                                                  (x', (y' - pVelo*dt))
+                                                  xs dt
+                                      DownDir  -> transDirection
+                                                  (x', (y' + pVelo*dt))
+                                                  xs dt
+                                      _ ->  transDirection (x',y') xs dt
 transDirection (x, y) [] _ = (CInt(floor x), CInt (floor y))
 
--- TEMP SECTION
-modelPosition :: ActiveKeys -> CenterPosition -> Double -> CenterPosition
+-- | Temporary function. Calculates the new position based on pressed keys
+modelPosition :: ActiveKeys     -- ^ Set of pressed keys
+              -> CenterPosition -- ^ Center position in the last frame
+              -> Double         -- ^ Delta time
+              -> CenterPosition -- ^ New center position
 modelPosition keys !(u,i) dt = calcPos (u, i) (transDirection (0.0, 0.0) dirs dt)
   where
     dirs = transformSet keys transformKeys
--- src/Sample/GameState.hs
+++ src/Sample/GameState.hs
@@ -42,149 +42,196 @@ import Engine.Collision
 import Engine.Consts
 import GameData
 
-
-
-
-
+-- |
 data GameState = GameState
-                 { level :: Int
-                 , world :: W.World
-                 , maps :: Map Int TileMap -- lvl -> map
-                 }
+    { level :: Int              -- ^ current level
+    , world :: W.World          -- ^ world
 
-getTilesModels :: GameState -> [RenderModel]
-getTilesModels (GameState lvl _ maps) = case Map.lookup lvl maps of
-  Just tileMap -> getModels (tiles tileMap) []
-  Nothing -> error "Level map not found!"
+    -- |  Map containing tilesets for every level
+    -- Key stores level number, beginning from 1.
+    , maps  :: Map Int TileMap
+    }
 
---change
-changeWorld :: GameState -> Camera -> W.Player -> W.World
-changeWorld (GameState lvl world _) cam player = W.updateWorld
-                                                    world cam player lvl
-
-updateMap :: GameState -> TileMap -> Map Int TileMap
-updateMap !(GameState lvl _ maps) !tiles = if isEmpty tiles
-                                           then maps
-                                           else
-                                             Map.insert lvl tiles maps
-
-changeTilesLvl :: GameState -> Camera -> TileMap
-changeTilesLvl (GameState lvl _ maps) cam = if hasOffsetChanged cam
-                                            then
-                                              case Map.lookup lvl maps of
-                                                Just tileMap -> changeTiles cam tileMap
-                                                Nothing -> error "Level map not found!"
-                                            else empty
-
-changeTiles :: Camera -> TileMap -> TileMap
-changeTiles !cam (TileMap w h tiles path) = TileMap w h (map (changeTile cam) tiles)
-                                           path
-
-changeTile :: Camera -> Tile -> Tile
-changeTile  (SDL.Rectangle (P(V2 camX camY)) _)
-  (Tile d p k !rm@(RenderModel _ (x, y) _ _ _ instr) !box) =
-   Tile { GD.dim = d, GD.pos = p, kind = k, model = rm
-                                                    { RM.pos = (CInt x', CInt y')
-                                                    , renderInstr = modifyPos
-                                                      instr [] (CInt x', CInt y')
-                                                    }, tBox = box}
-  where (x', y') = (fromIntegral (x - camX), fromIntegral (y - camY))
-
-getWorldModels :: GameState -> [RenderModel]
-getWorldModels (GameState lvl wor _) = W.renderWorld wor lvl
-
-getBoundingBoxes :: GameState -> [(BoundingBox, BoxKind)]
-getBoundingBoxes (GameState lvl world' maps') = case Map.lookup lvl maps' of
-  Just tileMap -> W.getEntitiesBoxes world' lvl ++ getTilesBox tileMap
-  Nothing -> error "Level map not found!"
-
-getPlayer :: GameState -> W.Player
-getPlayer (GameState _ world _) = W.player world
-
-levelInfo :: GameState -> (Int, Int)
-levelInfo (GameState lvl _ maps) = (w, h)
-  where (TileMap w h _ _) = case Map.lookup lvl maps of
-          Just tileMap -> tileMap
-          Nothing -> error "Level map not found!"
-
-getModelKey :: Int -> Map Int RenderModel-> RenderModel
-getModelKey n modMap = case Map.lookup n modMap of
-  Just m -> m
-  Nothing -> dummyModel
-
-modifyGameState :: Map Int TileMap -> W.World -> GameState -> GameState
-modifyGameState  correctedTiles wor' (GameState lvl _ _) =
-  GameState{ level = lvl, world = wor', maps = correctedTiles}
-
-getLevelSize :: GameState -> (CInt, CInt)
-getLevelSize (GameState lvl _ maps) =
-  let (TileMap w h tiles' tilesP) = case Map.lookup lvl maps of
-        Just tileMap -> tileMap
-        Nothing -> error "Level map not found!"
-      in (CInt(fromIntegral w), CInt (fromIntegral h))
-
-calcSum :: Camera -> (CInt, CInt) -> (Double, Double)
-calcSum (SDL.Rectangle (P(V2 camX camY)) _) (x, y) =
-  (fromIntegral(camX + x), fromIntegral (camY + y))
-
-getWorldAndTiles :: GameState -> (W.World, TileMap)
-getWorldAndTiles (GameState lvl world tileMaps) = (world, tileMap)
-  where tileMap = case Map.lookup (lvl-1) tileMaps of
-          Just tMap -> tMap
-          Nothing -> error "Level map not found!"
-
-getTile :: [(BoundingBox, BoxKind)] -> TileMap -> [Tile]
-getTile [] _ = [(Tile (0, 0) (0, 0) Sky undefined undefined)]
-getTile ((box, _):xs) (TileMap _ _ tiles _) = filter (\y -> tBox y == box) tiles
-
-prepack :: (CInt, CInt) -> (Double, Double)
-prepack (x, y) = (fromIntegral x, fromIntegral y)
-
-calc :: (Double, Double) -> (Double, Double) -> (Double, Double)
-calc (x, y) (x2, y2) = (x2 - x, y2 - y)
-
-gameLoop :: IORef EngineState -> IORef GameState -> Double -> IO ()
-gameLoop es gs timeStep = do
-  engineState <- readIORef es
-  gameState <- readIORef gs
-  let
-      activeKeys = keys engineState
-      tree = insertElements (getBoundingBoxes gameState) (newQuadtree 1
-                                                          (winSetup engineState))
-      (oldWorld, oldTiles) = getWorldAndTiles gameState
-      (cam'', tiles', world') = W.runWorld activeKeys timeStep (camera engineState)
-                                oldTiles oldWorld (winSetup engineState)
-                                (getBoundingBoxes gameState)
-
-      playerMod = W.heroM $ getPlayer gameState
-      levelDims = levelInfo gameState
-      position = modelPosition activeKeys (renPos playerMod) timeStep
-      model' = modifyModelPos playerMod position levelDims
-               (camera engineState)
-      cam' = calcCameraPosition (camera engineState) model'
-             (getLevelSize gameState)
-      player = W.updatePlayer (getPlayer gameState) model' (prepack position)
-      --(calcSum cam' position)
-      -- quadtree + collisions + react
-      collisions = checkCollisions (W.pBox player)
-                   (retrieve (W.playerBox world) tree)
-      correctCam = checkOffset (camera engineState) cam'
-      tileslvl = changeTilesLvl gameState correctCam
-      tileMaps = updateMap gameState tileslvl
-      world = changeWorld gameState correctCam player
-  threadDelay 8000
---  D.traceIO(show $ hasOffsetChanged correctCam)
---  dist (W.pBox player) collisions
---  D.traceIO (show collisions)
---  D.traceIO (show (calc (W.pPos $ getPlayer gameState) (W.pPos player)))
---  D.traceIO (show (getTile collisions (tileslvl !! 0)))
-  writeIORef gs (modifyGameState tileMaps world gameState)
-  writeIORef es engineState{camera = cam'}
-
---later: read from json config file maybe?
+-- |
 initStateG :: GameState
 initStateG = GameState
              { level = 1
              , world = undefined
              , maps = Map.empty
              }
+
+-- | Retrieve graphic information about a tileset in current level.
+--   When the information cannot be restored, it rises an error.
+getTilesModels :: GameState      -- ^ current game state
+               -> [RenderModel]  -- ^ tiles graphic representation
+getTilesModels (GameState lvl _ maps) = case Map.lookup lvl maps of
+                                           Just tileMap -> getModels
+                                                          (tiles tileMap) []
+                                           Nothing      -> error
+                                                          "Level map not found!"
+
+-- |
+changeWorld :: GameState -> Camera -> W.Player -> W.World
+changeWorld (GameState lvl world _) cam player = W.updateWorld
+                                                 world cam player lvl
+
+-- |
+updateMap :: GameState -> TileMap -> Map Int TileMap
+updateMap !(GameState lvl _ maps) !tiles = if isEmpty tiles
+                                           then maps
+                                           else Map.insert lvl tiles maps
+
+-- |
+changeTilesLvl :: GameState -> Camera -> TileMap
+changeTilesLvl (GameState lvl _ maps) cam = if hasOffsetChanged cam
+                                            then
+                                              case Map.lookup lvl maps of
+                                                Just tileMap -> changeTiles cam
+                                                                tileMap
+                                                Nothing      -> error
+                                                                "Level map not\
+                                                                \ found!"
+                                            else empty
+
+-- |
+changeTiles :: Camera -> TileMap -> TileMap
+changeTiles !cam (TileMap w h tiles path) = TileMap w h (map (changeTile cam) tiles)
+                                           path
+
+-- | Define a new relative position for tile sprites in the scene.
+-- This function is evoked only when the camera offset has been set.
+changeTile :: Camera -> Tile -> Tile
+changeTile  (SDL.Rectangle (P(V2 camX camY)) _)
+  (Tile d p k !rm@(RenderModel _ (x, y) _ _ _ instr) !box) = Tile
+                                                             { GD.dim = d
+                                                             , GD.pos = p
+                                                             , kind = k
+                                                             , model = rm
+                                                               { RM.pos =
+                                                                 (CInt x', CInt y')
+                                                               , renderInstr =
+                                                                 modifyPos
+                                                                 instr []
+                                                                 (CInt x', CInt y')
+                                                               }
+                                                             , tBox = box
+                                                             }
+  where (x', y') = (fromIntegral (x - camX), fromIntegral (y - camY))
+
+-- |
+getWorldModels :: GameState -> [RenderModel]
+getWorldModels (GameState lvl wor _) = W.renderWorld wor lvl
+
+-- |
+getBoundingBoxes :: GameState -> [(BoundingBox, BoxKind)]
+getBoundingBoxes (GameState lvl world' maps') = case Map.lookup lvl maps' of
+                                                  Just tileMap -> W.getEntitiesBoxes
+                                                                  world' lvl
+                                                                  ++ getTilesBox
+                                                                  tileMap
+                                                  Nothing      -> error
+                                                                  "Level map not\
+                                                                  \ found!"
+
+-- |
+getPlayer :: GameState -> W.Player
+getPlayer (GameState _ world _) = W.player world
+
+-- |
+levelInfo :: GameState -> (Int, Int)
+levelInfo (GameState lvl _ maps) = (w, h)
+  where (TileMap w h _ _) = case Map.lookup lvl maps of
+                              Just tileMap -> tileMap
+                              Nothing      -> error "Level map not found!"
+
+-- |
+getModelKey :: Int -> Map Int RenderModel-> RenderModel
+getModelKey n modMap = case Map.lookup n modMap of
+                         Just m  -> m
+                         Nothing -> error "Model not found!"
+
+-- |
+modifyGameState :: Map Int TileMap -> W.World -> GameState -> GameState
+modifyGameState  correctedTiles wor' (GameState lvl _ _) = GameState
+                                                           { level = lvl
+                                                           , world = wor'
+                                                           , maps  = correctedTiles
+                                                           }
+
+-- |
+getLevelSize :: GameState -> (CInt, CInt)
+getLevelSize (GameState lvl _ maps) =
+  let (TileMap w h tiles' tilesP) = case Map.lookup lvl maps of
+                                      Just tileMap -> tileMap
+                                      Nothing      -> error "Level map \
+                                                            \not found!"
+  in (CInt(fromIntegral w), CInt (fromIntegral h))
+
+-- |
+calcSum :: Camera -> (CInt, CInt) -> (Double, Double)
+calcSum (SDL.Rectangle (P(V2 camX camY)) _) (x, y) = ( fromIntegral(camX + x)
+                                                     , fromIntegral (camY + y))
+
+-- |
+getWorldAndTiles :: GameState -> (W.World, TileMap)
+getWorldAndTiles (GameState lvl world tileMaps) = (world, tileMap)
+  where tileMap = case Map.lookup (lvl-1) tileMaps of
+                    Just tMap -> tMap
+                    Nothing   -> error "Level map not found!"
+
+-- |
+getTile :: [(BoundingBox, BoxKind)]
+        -> TileMap
+        -> [Tile]
+getTile [] _ = [(Tile (0, 0) (0, 0) Sky undefined undefined)]
+getTile ((box, _):xs) (TileMap _ _ tiles _) = filter (\y -> tBox y == box) tiles
+
+-- |
+prepack :: (CInt, CInt)
+        -> (Double, Double)
+prepack (x, y) = (fromIntegral x, fromIntegral y)
+
+-- |
+calc :: (Double, Double) -> (Double, Double) -> (Double, Double)
+calc (x, y) (x2, y2) = (x2 - x, y2 - y)
+
+-- |
+gameLoop :: IORef EngineState -- ^
+         -> IORef GameState   -- ^
+         -> Double            -- ^
+         -> IO ()             -- ^
+gameLoop es gs timeStep = do
+                       engineState <- readIORef es
+                       gameState <- readIORef gs
+                       let
+                         activeKeys = keys engineState
+                         tree = insertElements
+                                (getBoundingBoxes gameState)
+                                (newQuadtree 1 (winSetup engineState))
+                         (oldWorld, oldTiles) = getWorldAndTiles gameState
+                         (cam'', tiles', world') = W.runWorld activeKeys
+                                                 timeStep (camera engineState)
+                                                 oldTiles oldWorld
+                                                 (winSetup engineState)
+                                                 (getBoundingBoxes gameState)
+
+                         playerMod = W.heroM $ getPlayer gameState
+                         levelDims = levelInfo gameState
+                         position = modelPosition activeKeys (renPos playerMod)
+                                    timeStep
+                         model' = modifyModelPos playerMod position levelDims
+                                  (camera engineState)
+                         cam' = calcCameraPosition (camera engineState) model'
+                                (getLevelSize gameState)
+                         player = W.updatePlayer (getPlayer gameState) model'
+                                  (prepack position)
+                         collisions = checkCollisions (W.pBox player)
+                                      (retrieve (W.playerBox world) tree)
+                         correctCam = checkOffset (camera engineState) cam'
+                         tileslvl = changeTilesLvl gameState correctCam
+                         tileMaps = updateMap gameState tileslvl
+                         world = changeWorld gameState correctCam player
+                       threadDelay 8000
+
+                       writeIORef gs (modifyGameState tileMaps world gameState)
+                       writeIORef es engineState{ camera = cam' }
